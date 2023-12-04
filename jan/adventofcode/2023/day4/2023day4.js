a485465f564a1298c54bf9396da26c0688af2441
P13TJ3N <55795954+P13TJ3N@users.noreply.github.com>
2023-12-04 23:02:09 +0000

    finished day 4.2
    finished day 4.2, it works well!

--- jan/adventofcode/2023/day4/2023day4.js
+++ jan/adventofcode/2023/day4/2023day4.js
@@ -21,31 +21,44 @@ function arrSum(arr) {
 
 function processInputText(inputText) {
     var lines = inputText.replace(/;/g,",").split('\n'); //replacing all semicolons with commas to split lines by commas
+    var keerSpelen = [];
+    console.log(lines.length);
+    
+    for (let i = 1; i < lines.length+1; i++){keerSpelen[i] = 1;};
+    console.log(keerSpelen);
 
     lines.forEach(function(line) {
-        console.log(line);
         let cleanLine = line.trim().split(/[\s,\t,\n]+/).join(' ');
-        console.log(cleanLine);
         let splitline = cleanLine.split(': '); //seperate game prefix
         let gameId = splitline[0].replace('Card ','');
         let sides = splitline[1].split(' | ');
         let winningNumbers = sides[0].split(" ");
         let checkingNumbers = sides[1].split(" ");
+
+        let matchesOpDezeKaart = 0;
         let score = 0;
+
         checkingNumbers.forEach(function(number) {
             console.log(`bestaat ${number} in "${winningNumbers}"`);
             if(winningNumbers.includes(number)){
                 if(score==0){score++} else{
                     score = score*2;
                 }
-                console.log(`yep, score is now ${score}`);
-            }else{
-                console.log('nope')
+                matchesOpDezeKaart++;
             };
         });
+
+        console.log(`Kaart${gameId}:  Aantal matches ${matchesOpDezeKaart}`);
+        
+        for (let i = 1; i < matchesOpDezeKaart+1; i++){
+            huidigeKaart = parseInt(gameId) + i; 
+            keerSpelen[huidigeKaart] += keerSpelen[parseInt(gameId)];
+        };
         resultsList.push(score);
+        resultsList2.push(parseInt(keerSpelen[parseInt(gameId)]));
+        console.log(resultsList2);
     });
-    // resultsList2.push(parseInt(cardAmount));
+    
 }
 
 function displayResults() {
@@ -55,5 +68,5 @@ function displayResults() {
     console.log(`${tekst2} : ${resultsListSum2}`);
     //stuur info naar HTML document output
     var outputElement = document.getElementById("outputText");
-    outputElement.textContent = `${tekst} : ${resultsListSum} \n ${tekst2} : ${resultsListSum2}`;
+    outputElement.textContent = `${tekst} : ${resultsListSum} \n ${tekst2} : Er zijn ${resultsListSum2} kaarten`;
 };
