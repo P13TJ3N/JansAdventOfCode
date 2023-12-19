c8f4277e23ec8461fc8a57cd2c5fbb1c63b4d771
P13TJ3N <55795954+P13TJ3N@users.noreply.github.com>
2023-12-19 23:25:50 +0000

    day 11.2 bruteforce method
    day 11.2 bruteforce method

--- jan/adventofcode/2023/day11/2023day11.js
+++ jan/adventofcode/2023/day11/2023day11.js
@@ -2,7 +2,7 @@
 let resultsList = []; //vind de verste locatie die het diertje kan lopen in de loep van de start.
 let resultsList2 = [];
 const tekst = "som van korste afstanden tussen planeten is";
-const tekst2 = "";
+const tekst2 = "de som van de kortste mega afstanden is";
 
 function calculateResult() {
     // haal Input waarden op
@@ -35,20 +35,22 @@ function expandSpace(lines,grootte){
 }
 
 function processInputText(inputText) {
-    let score = 0;
     var lines = inputText.split('\n');
-    let planeten = [];
+
     for (let index = 0; index < lines.length; index++) {
         lines[index] = lines[index].split("");
-    }
+    };
+    let lines2 = lines;
+
+    //============================11.1====================
     //GROEI DE RUIMTE
-    //https://stackoverflow.com/questions/15170942/how-to-rotate-a-matrix-in-an-array-in-javascript
-    lines = lines[0].map((val, index) => lines.map(row => row[index]).reverse())//kantel 90 graden
+    lines = lines[0].map((val, index) => lines.map(row => row[index]).reverse())//kantel 90 graden -    //https://stackoverflow.com/questions/15170942/how-to-rotate-a-matrix-in-an-array-in-javascript
     lines = expandSpace(lines,1);
     lines = lines[0].map((val, index) => lines.map(row => row[row.length-1-index]))//kantel terug
     lines = expandSpace(lines,1);
     
-    //tel planeten    
+    //tel planeten
+    let planeten = [];    
     for (let row = 0; row < lines.length; row++) {
         if(lines[row].includes("#")){
             for (let col = 0; col < lines[0].length; col++) {
@@ -62,9 +64,8 @@ function processInputText(inputText) {
         for (let planeet2 = 0; planeet2 < planeten.length; planeet2++) {
             if(planeet < planeet2){planeetParen.push([planeet,planeet2])}
         };};
-    // console.log(planeten);
-    // console.log(planeetParen);
 
+    // meet afstand
     for (let paarNr = 0; paarNr < planeetParen.length; paarNr++) {
         let afstand = 0;
         const startRij = planeten[planeetParen[paarNr][0]][0];
@@ -74,7 +75,50 @@ function processInputText(inputText) {
         afstand = Math.abs(startCol - doelCol) +  Math.abs(startRij - doelRij);
         resultsList.push(afstand);
     }
+
+    //============================11.2====================
+    const groeiFactor = 1000000;
+
+    //GROEI DE RUIMTE
+    console.log("start kantelen");
+    lines2 = lines2[0].map((val, index) => lines2.map(row => row[index]).reverse())//kantel 90 graden -    //https://stackoverflow.com/questions/15170942/how-to-rotate-a-matrix-in-an-array-in-javascript
+    console.log("klaar met kantelen, we voegen nu tering veel ruimte toe");
+    lines2 = expandSpace(lines2,groeiFactor);
+    console.log("start terug kantelen");
+    lines2 = lines2[0].map((val, index) => lines2.map(row => row[row.length-1-index]))//kantel terug
+    console.log("klaar met terug kantelen, we voegen nu tering veel ruimte toe");
+    lines2 = expandSpace(lines2,groeiFactor);
     
+    console.log(`we gaan nu de planeten tellen`);
+    //tel planeten
+    let planeten2 = [];    
+    for (let row = 0; row < lines2.length; row++) {
+        if(lines2[row].includes("#")){
+            for (let col = 0; col < lines2[0].length; col++) {
+                const ruimte = lines2[row][col];
+                if(ruimte === "#"){planeten2.push([row,col]);
+    };};};};
+
+    console.log(`we gtellen nu de unieke planeet combinaties`);
+    // zie welke unieke combinaties van planeten er zijn
+    let planeetParen2 = []
+    for (let planeet = 0; planeet < planeten2.length; planeet++) {
+        for (let planeet2 = 0; planeet2 < planeten2.length; planeet2++) {
+            if(planeet < planeet2){planeetParen2.push([planeet,planeet2])}
+        };};
+
+    console.log(`we meten nu de afstanden tussen de planeten`);
+    // meet afstand
+    for (let paarNr = 0; paarNr < planeetParen2.length; paarNr++) {
+        let afstand = 0;
+        const startRij = planeten2[planeetParen2[paarNr][0]][0];
+        const startCol = planeten2[planeetParen2[paarNr][0]][1];
+        const doelRij = planeten2[planeetParen2[paarNr][1]][0];
+        const doelCol = planeten2[planeetParen2[paarNr][1]][1];
+        afstand = Math.abs(startCol - doelCol) +  Math.abs(startRij - doelRij);
+        resultsList2.push(afstand);
+    }
+
     //RENDER RESULT
     let renderMap = lines;
     for (let index = 0; index < lines.length; index++) {
