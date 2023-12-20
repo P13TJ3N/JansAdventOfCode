4b7cec2dd9d6cfac1aec24f851988c722fbcf6eb
P13TJ3N <55795954+P13TJ3N@users.noreply.github.com>
2023-12-20 17:34:24 +0000

    finished day 11!
    updated it with an approach that doenst brute force it

--- jan/adventofcode/2023/day11/2023day11.js
+++ jan/adventofcode/2023/day11/2023day11.js
@@ -1,4 +1,3 @@
-
 let resultsList = []; //vind de verste locatie die het diertje kan lopen in de loep van de start.
 let resultsList2 = [];
 const tekst = "som van korste afstanden tussen planeten is";
@@ -20,18 +19,15 @@ function arrSum(arr) {
     }, 0);
 }
 
-function expandSpace(lines,grootte){
-    let expandedLines = lines;
-    for (let index = 0; index < expandedLines.length; index++) {
-        const line = expandedLines[index];
-        if(!(line.includes("#"))){ //voeg nieuwe lijn toe als er geen planeten zij
-            for (let ruimte = 0; ruimte < grootte; ruimte++) {
-                expandedLines.splice(index,0,expandedLines[index]); 
-            };
-            index+=grootte;// sla lijnen over
+function vindLegeRijen(lines){
+    let legeRijen = [];
+    for (let index = 0; index < lines.length; index++) {
+        const line = lines[index];
+        if(!(line.includes("#"))){ //registreer de lege lijn
+            legeRijen.push(index);
         };
     }
-    return expandedLines;
+    return legeRijen;
 }
 
 function processInputText(inputText) {
@@ -40,14 +36,14 @@ function processInputText(inputText) {
     for (let index = 0; index < lines.length; index++) {
         lines[index] = lines[index].split("");
     };
-    let lines2 = lines;
 
-    //============================11.1====================
+    const groeiFactor = 1000000;
+
     //GROEI DE RUIMTE
     lines = lines[0].map((val, index) => lines.map(row => row[index]).reverse())//kantel 90 graden -    //https://stackoverflow.com/questions/15170942/how-to-rotate-a-matrix-in-an-array-in-javascript
-    lines = expandSpace(lines,1);
+    ruimteColomen = vindLegeRijen(lines);
     lines = lines[0].map((val, index) => lines.map(row => row[row.length-1-index]))//kantel terug
-    lines = expandSpace(lines,1);
+    ruimteRijen = vindLegeRijen(lines);
     
     //tel planeten
     let planeten = [];    
@@ -68,57 +64,25 @@ function processInputText(inputText) {
     // meet afstand
     for (let paarNr = 0; paarNr < planeetParen.length; paarNr++) {
         let afstand = 0;
+        let afstand2 = 0;
+
+        let ruimteRijMatches = [];
+        let ruimteColomMatches = [];
         const startRij = planeten[planeetParen[paarNr][0]][0];
-        const startCol = planeten[planeetParen[paarNr][0]][1];
         const doelRij = planeten[planeetParen[paarNr][1]][0];
+        const startCol = planeten[planeetParen[paarNr][0]][1];
         const doelCol = planeten[planeetParen[paarNr][1]][1];
-        afstand = Math.abs(startCol - doelCol) +  Math.abs(startRij - doelRij);
+
+        if(startRij < doelRij){ruimteRijMatches = ruimteRijen.filter(function(item) {return (item > startRij && item < doelRij);});}
+        else{ruimteRijMatches = ruimteRijen.filter(function(item) {return (item > doelRij && item < startRij);});}
+        if(startCol < doelCol){ruimteColomMatches = ruimteColomen.filter(function(item){return (item > startCol && item < doelCol);});}
+        else{ruimteColomMatches = ruimteColomen.filter(function(item){return (item > doelCol && item < startCol);});}
+
+        afstand =  Math.abs(startCol - doelCol) +  Math.abs(startRij - doelRij) + ruimteRijMatches.length*1 + ruimteColomMatches.length*1;
+        afstand2 =  Math.abs(startCol - doelCol) +  Math.abs(startRij - doelRij) + (ruimteRijMatches.length*(groeiFactor-1)) + (ruimteColomMatches.length*(groeiFactor-1));
         resultsList.push(afstand);
+        resultsList2.push(afstand2);
     }
-
-    //============================11.2====================
-    const groeiFactor = 1000000;
-
-    //GROEI DE RUIMTE
-    console.log("start kantelen");
-    lines2 = lines2[0].map((val, index) => lines2.map(row => row[index]).reverse())//kantel 90 graden -    //https://stackoverflow.com/questions/15170942/how-to-rotate-a-matrix-in-an-array-in-javascript
-    console.log("klaar met kantelen, we voegen nu tering veel ruimte toe");
-    lines2 = expandSpace(lines2,groeiFactor);
-    console.log("start terug kantelen");
-    lines2 = lines2[0].map((val, index) => lines2.map(row => row[row.length-1-index]))//kantel terug
-    console.log("klaar met terug kantelen, we voegen nu tering veel ruimte toe");
-    lines2 = expandSpace(lines2,groeiFactor);
-    
-    console.log(`we gaan nu de planeten tellen`);
-    //tel planeten
-    let planeten2 = [];    
-    for (let row = 0; row < lines2.length; row++) {
-        if(lines2[row].includes("#")){
-            for (let col = 0; col < lines2[0].length; col++) {
-                const ruimte = lines2[row][col];
-                if(ruimte === "#"){planeten2.push([row,col]);
-    };};};};
-
-    console.log(`we gtellen nu de unieke planeet combinaties`);
-    // zie welke unieke combinaties van planeten er zijn
-    let planeetParen2 = []
-    for (let planeet = 0; planeet < planeten2.length; planeet++) {
-        for (let planeet2 = 0; planeet2 < planeten2.length; planeet2++) {
-            if(planeet < planeet2){planeetParen2.push([planeet,planeet2])}
-        };};
-
-    console.log(`we meten nu de afstanden tussen de planeten`);
-    // meet afstand
-    for (let paarNr = 0; paarNr < planeetParen2.length; paarNr++) {
-        let afstand = 0;
-        const startRij = planeten2[planeetParen2[paarNr][0]][0];
-        const startCol = planeten2[planeetParen2[paarNr][0]][1];
-        const doelRij = planeten2[planeetParen2[paarNr][1]][0];
-        const doelCol = planeten2[planeetParen2[paarNr][1]][1];
-        afstand = Math.abs(startCol - doelCol) +  Math.abs(startRij - doelRij);
-        resultsList2.push(afstand);
-    }
-
     //RENDER RESULT
     let renderMap = lines;
     for (let index = 0; index < lines.length; index++) {
