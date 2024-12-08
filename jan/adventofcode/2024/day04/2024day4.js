ff719a1fe6049247ddb21b715721ab2275fda6e8
P13TJ3N <55795954+P13TJ3N@users.noreply.github.com>
2024-12-08 10:22:30 +0000

    finished day 4.1 and 4.2!
    Kinda proud of part1

--- jan/adventofcode/2024/day04/2024day4.js
+++ jan/adventofcode/2024/day04/2024day4.js
@@ -1,7 +1,7 @@
 let resultsList = [];
 let resultsList2 = [];
-const tekst = "";
-const tekst2 = "";
+const tekst = "Aantal XMAS in the kruiswoord puzzel";
+const tekst2 = "Aantal X-MAS in de 'kruis'woord puzzel";
 
 function calculateResult() {
     var inputElement = document.getElementById("inputTextArea");// haal Input waarden op
@@ -15,39 +15,79 @@ function arrSum(arr) {
         return a + b;
     }, 0);
 }
-function validDigits(n){ //https://stackoverflow.com/questions/7148513/javascript-regular-expression-allow-only-numbers-and-commas
-    return n.replace(/[^\d,]+/g, '');
- }
+function findXmas(arr){
+    const xmasFinder = /XMAS/g //https://regexr.com/
+    const samxFinder = /SAMX/g
+    let matches = 0 
+    for (let line = 0; line < arr.length; line++){
+        let xmatches = arr[line].match(xmasFinder);
+        let smatches = arr[line].match(samxFinder);
+        if(xmatches){matches+=xmatches.length; console.log(`XMAS gevonden: ${xmatches}`);};
+        if(smatches){matches+=smatches.length; console.log(`SAMX gevonden: ${smatches}`);};
+    }
+    return matches;
+}
+ function arrDraai45(arrString) {// dit gaat alleen werken als de array al een 2d rechthoek is 
+    let arr = [];
+    for (let line = 0; line < arrString.length; line++){arr.push(arrString[line].split(''))};
+    let arr45 = [];
+    let hoogte = arr.length;
+    let breedte = arr[0].length;
+    let aantalEenheden = hoogte * breedte;
+    let x = 0;
+    let y = 0;
+    let rij = [];
+    for (let eenheid = 0; eenheid < aantalEenheden; eenheid++){
+        rij.push(arr[x][y]);
+        if(x>0 && y<breedte-1){
+            x--; y++;
+        }else{
+            arr45.push(rij.join("")); rij =[];
+            if(arr45.length < hoogte){
+                x = arr45.length; y = 0;
+            }else{
+                x = hoogte-1; y = (arr45.length - x);
+            };
+        };
+    };
+    return arr45
+}
 
-//main loop
 function processInputText(inputText) {
+    //opdracht 1
     var lines = inputText.split('\n')
+
     let lines90 = []
     for (let line = 0; line < lines.length; line++){lines90.push(lines[line].split(''))}
-    lines90 = lines90[0].map((val, index) => lines90.map(row => row[index]).reverse().join(""))//yeet 90 degrees
-    console.log(lines90);
+    lines90 = lines90[0].map((val, index) => lines90.map(row => row[index]).reverse().join(""))//draai 90 graden
 
-    //XMASAMX vind maar 1 resultaat met een OR regex :(
+    let arr45 = arrDraai45(lines);
+    let arr135 = arrDraai45(lines90);
 
     xmasLijst = []
-    //opdracht1
-    for (let line = 0; line < lines.length; line++){
-        const xamsFinder = /XMAS/g //https://regexr.com/
-        const samxFinder = /SAMX/g
-        let xmatches = lines[line].match(xamsFinder);
-        let xmatches90 = lines90[line].match(xamsFinder);
-        let smatches = lines[line].match(samxFinder);
-        let smatches90 = lines90[line].match(samxFinder);
-        if(xmatches){xmasLijst.push(xmatches)};
-        if(xmatches90){xmasLijst.push(xmatches90)};
-        if(smatches){xmasLijst.push(smatches)};
-        if(smatches90){xmasLijst.push(smatches90)};
+    resultsList.push(findXmas(lines));
+    resultsList.push(findXmas(arr45));
+    resultsList.push(findXmas(lines90));
+    resultsList.push(findXmas(arr135));
+
+    //opdracht 2
+    let lines2 = [];
+    for (let line = 0; line < lines.length; line++){lines2.push(lines[line].split(''))};
+    for (let rij = 1; rij < lines2.length-1; rij++){// skip colom 0 en rij 0, stop 1 eerder
+        for (let kol = 1; kol < lines2[rij].length-1; kol++){
+            if(lines2[rij][kol] === 'A'){
+                    if(
+                        ((lines2[rij-1][kol-1] === 'M' && lines2[rij+1][kol+1] === 'S') ||
+                        (lines2[rij-1][kol-1] === 'S' && lines2[rij+1][kol+1] === 'M')) &&
+
+                        ((lines2[rij+1][kol-1] === 'M' && lines2[rij-1][kol+1] === 'S') ||
+                        (lines2[rij+1][kol-1] === 'S' && lines2[rij-1][kol+1] === 'M'))
+                    ){resultsList2.push(1)};
+            };
+        };
     };
-
-    console.log(xmasLijst);
-
-
-    let kaart = ':D'
+    console.log(lines2);
+    let kaart = resultsList;
     renderKaart(kaart);
 }
 
