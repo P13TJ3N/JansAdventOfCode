9ea7adead39ea36c842352ea250d493c45f2fcec
P13TJ3N <55795954+P13TJ3N@users.noreply.github.com>
2025-12-29 19:27:23 +0000

    finished day 4!
    finished day 4!

--- jan/adventofcode/2025/day4.py
+++ jan/adventofcode/2025/day4.py
@@ -3,7 +3,7 @@ input = (open(input_csv))
 input = input.read().strip().split()
 balen = []
 start_baal = []
-oppakbare_toiletpappier_balen = 0
+totaal_oppakbare_toiletpappier_balen = 0
 
 ##################### Input prep #####################
 for i in range(len(input[0])+2):
@@ -37,32 +37,37 @@ def check_buren(balen, row, col, buren_doel):
     for plaats in plaatsen:
         buurplek = plaats[0] + row
         buurplek2 = plaats[1] + col
-        print(balen[buurplek][buurplek2])
         if balen[buurplek][buurplek2] == '@':
             buren += 1
-            # print("Buur gevonden op: ", buurplek, buurplek2, "aantal buren is nu", buren)
     if buren_doel > buren:
-        print("Aantal buren:", buren, "is minder dan doel:", buren_doel)
         check = True
-        print(check)
     return check
+
+def itteratorinator(balen, buren_doel, start_index):
+    oppakbare_toiletpappier_balen = 0
+    balen_resultaat = [row[:] for row in balen] # https://stackoverflow.com/questions/17873384/how-to-deep-copy-a-list
+    for col_index in range(len(balen)-2):
+        for row_index in range(len(balen)-2):
+            if balen[col_index+start_index][row_index+start_index] == '@':
+                if(check_buren(balen, col_index+start_index, row_index+start_index, buren_doel)):
+                    oppakbare_toiletpappier_balen += 1
+                    balen_resultaat[col_index+start_index][row_index+start_index] = 'X'
+    return oppakbare_toiletpappier_balen, balen_resultaat
 
 ##################### Main #####################
 balen_resultaat = [row[:] for row in balen] # https://stackoverflow.com/questions/17873384/how-to-deep-copy-a-list
 
 buren_doel = 4
-for col_index in range(len(balen)-2):
-    for row_index in range(len(balen)-2):
-        if balen[col_index+start_index][row_index+start_index] == '@':
-            print("cecking:", col_index+start_index, row_index+start_index)
-            if(check_buren(balen, col_index+start_index, row_index+start_index, buren_doel)):
-                oppakbare_toiletpappier_balen += 1
-                balen_resultaat[col_index+start_index][row_index+start_index] = 'X'
+resultaat = itteratorinator(balen, buren_doel, start_index)
+score = resultaat[0]
+balen = [row[:] for row in resultaat[1]]
+totaal_oppakbare_toiletpappier_balen += score
+print(score)
 
-print(oppakbare_toiletpappier_balen)
+while score > 0:
+    resultaat = itteratorinator(balen, buren_doel, start_index)
+    score = resultaat[0]
+    balen = [row[:] for row in resultaat[1]]
+    totaal_oppakbare_toiletpappier_balen += score
 
-# for baal_copie in balen_resultaat:
-#     print(''.join(baal_copie))
-
-# for baal in balen:
-#     print(''.join(baal))
+print(totaal_oppakbare_toiletpappier_balen)
